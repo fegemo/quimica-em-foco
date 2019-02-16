b4663403b8b366c6414bf807abc286014c6dca8c
Flavio Coutinho <fegemo@gmail.com>
2019-02-15 23:18:39 -0200

    Adds different images to the pages and a scrolling bg to header

--- src/app/page-list/page-list.component.ts
+++ src/app/page-list/page-list.component.ts
@@ -1,20 +1,24 @@
-import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
-import { AngularFirestore } from '@angular/fire/firestore';
+import { Component, OnInit } from "@angular/core";
+import { Observable } from "rxjs";
+import { startWith, tap } from "rxjs/operators";
+import { AngularFirestore } from "@angular/fire/firestore";
 
 @Component({
-  selector: 'app-page-list',
-  templateUrl: './page-list.component.html',
-  styleUrls: ['./page-list.component.css']
+  selector: "app-page-list",
+  templateUrl: "./page-list.component.html",
+  styleUrls: ["./page-list.component.css"]
 })
 export class PageListComponent implements OnInit {
-
   pages: Observable<any[]>;
   constructor(db: AngularFirestore) {
-    this.pages = db.collection('pages').valueChanges();
+    this.pages = db
+      .collection("pages")
+      .valueChanges()
+      .pipe(
+        tap(pages => sessionStorage.setItem("pages", JSON.stringify(pages))),
+        startWith(JSON.parse(sessionStorage.getItem("pages")))
+      );
   }
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
